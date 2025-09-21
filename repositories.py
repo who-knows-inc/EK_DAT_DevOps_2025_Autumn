25831586c5c890f259f739b75bce3ca623b2ae6c
TheOriginalJiozx <160468819+TheOriginalJiozx@users.noreply.github.com>
2025-09-21 12:09:07 +0200

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -52,10 +52,10 @@ GROUP_REPOS = [
   {
     "name": "devkopa",
     "gitLinks": ["https://github.com/devkopa/WhoKnows-legacy", "https://github.com/devkopa/WhoKnows-next-gen"],
-    "backend": "",
-    "frontend": "",
+    "backend": "http://20.251.217.144:3000/",
+    "frontend": "http://20.251.217.144:3000/",
     "monitoring": "",
-    "stack": [],
+    "stack": ["Ruby", "Rails", "PostgreSQL", "bcrypt", "puma"],
     "documentation": [],
     "sla": "",
   },
