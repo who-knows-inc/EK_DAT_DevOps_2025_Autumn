491928ebd92b10641c0850c70932ea2c7158af84
anderslatif <anderslatif@users.noreply.github.com>
2025-11-06 13:41:19 +0100

    Created the database migrations project and fixed related slides

---
+++ 08._Databases_Migrations/01._node_knex/knexfile.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+/**
+ * @type { import("knex").Knex.Config }
+ */
+export default {
+  client: 'postgresql',
+  connection: {
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
